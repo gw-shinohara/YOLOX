e65b2ac4edfce8cdb5e04362baea35ae14d69e9f
gw-shinohara <shinohara@globalwalkers.co.jp>
2023-09-29 14:33:50 +0900

    [Update] Convert scripts

--- example/convert_onnx/convert_onnx.sh
+++ example/convert_onnx/convert_onnx.sh
@@ -5,8 +5,18 @@ cd $prj_dir
 
 exp_name="grasped_yolox_s"
 exp_path="$prj_dir/exps/custom/$exp_name.py"
+output_dir="$prj_dir/YOLOX_outputs/grasped_yolox_s/onnx"
+mkdir $output_dir
 
-weights_path="/home/shinohara/Documents/YOLOX/YOLOX_outputs/grasped_yolox_s_230919/grasped_yolox_s.pth"
+model_name=best_ckpt
 
 #Note that to convert to openvino, opeset must be set to 10.
-python3 $prj_dir/tools/export_onnx.py --output-name "$exp_name.onnx" -f $exp_path -c $weights_path --opset 10
+
+# for model_name in best_ckpt epoch_10_ckpt epoch_11_ckpt epoch_12_ckpt epoch_13_ckpt epoch_14_ckpt epoch_15_ckpt epoch_1_ckpt epoch_2_ckpt epoch_3_ckpt epoch_4_ckpt epoch_5_ckpt epoch_6_ckpt epoch_7_ckpt epoch_8_ckpt epoch_9_ckpt last_epoch_ckpt latest_ckpt
+# { 
+# echo $model_name
+# weights_path="$prj_dir/YOLOX_outputs/grasped_yolox_s/$model_name.pth"
+# output_path="$output_dir/$model_name.onnx"
+python3 $prj_dir/tools/export_onnx.py -f $exp_path -c $weights_path --output-name $output_path --opset 10
+# }
+
--- example/convert_onnx/convert_openvino.sh
+++ example/convert_onnx/convert_openvino.sh
@@ -1,12 +1,20 @@
 #/bin/bash
 prj_dir=/home/shinohara/Documents/YOLOX
 # args
-exp_name="grasped_yolox_s"
-weights_dir=$prj_dir/YOLOX_outputs/grasped_yolox_s_230919
-output_dir="$weights_dir/openvino"
-mkdir $output_dir
-weights_onnx_path="$weights_dir/$exp_name.onnx"
+weights_dir=$prj_dir/YOLOX_outputs/grasped_yolox_s/onnx
+output_dir="$prj_dir/YOLOX_outputs/grasped_yolox_s/openvino"
+
 input_shapes="[1,3,640,640]"
 optimizer_dir="/opt/intel/openvino_2021/deployment_tools/model_optimizer"
 source $setupvars_file
-python3 $optimizer_dir/mo.py --input_model $weights_onnx_path --input_shape $input_shapes --output_dir $output_dir
+
+model_name="best_ckpt"
+
+# for model_name in best_ckpt epoch_10_ckpt epoch_11_ckpt epoch_12_ckpt epoch_13_ckpt epoch_14_ckpt epoch_15_ckpt epoch_1_ckpt epoch_2_ckpt epoch_3_ckpt epoch_4_ckpt epoch_5_ckpt epoch_6_ckpt epoch_7_ckpt epoch_8_ckpt epoch_9_ckpt last_epoch_ckpt latest_ckpt
+# {
+# weights_path="$weights_dir/$model_name.onnx"
+# output_model_dir="$output_dir/$model_name"
+# mkdir $output_model_dir
+# echo $output_model_dir
+python3 $optimizer_dir/mo.py --input_model $weights_path --input_shape $input_shapes --output_dir $output_model_dir
+# }
